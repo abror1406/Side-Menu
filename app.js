36dbd6b85b8ce85c4fd5c9f9d9172efd5c0438f5
abror yuldashev <yuldashev3787@gmail.com>
2024-07-19 23:14:27 +0500

    full side menu

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-const searchButton = document.querySelector('.search');
-const modalPanel = document.querySelector('.modal-panel');
-const panelButton = document.querySelector('.panel-button');
+const searchButton = document.querySelector('.search-button');
+const runButton = document.querySelector('.run-button');
+const modalBg = document.querySelector('.modalbg');
+const modalRun = document.querySelector('.two');
+const modal = document.querySelector('.modal');
+const panelButton = document.querySelector('.panel__button');
+const panelButtonTwo = document.querySelector('.panel__buttonTwo');
 
-const modalPanelRun = document.querySelector('.runPanel');
-const RunButton = document.querySelector('.run');
+runButton.addEventListener('click', function(){
+    if (modalRun.classList.contains('active')) {
+        modalRun.classList.remove('active');
+    } else {
+        modalRun.classList.add('active');
+    }
+    if (modalBg.classList.contains('active')) {
+        modalBg.classList.remove('active');
+    } else {
+        modalBg.classList.add('active');
+    } 
+})
 
 searchButton.addEventListener('click', function() {
-    if (modalPanel.classList.contains('active')) {
-        modalPanel.classList.remove('active');
+    if (modal.classList.contains('active')) {
+        modal.classList.remove('active');
     } else {
-        modalPanel.classList.add('active');
+        modal.classList.add('active');
     }
-});
-RunButton.addEventListener('click', function() {
-    if (modalPanelRun.classList.contains('active')) {
-        modalPanelRun.classList.remove('active');
+    if (modalBg.classList.contains('active')) {
+        modalBg.classList.remove('active');
     } else {
-        modalPanelRun.classList.add('active');
-    }
-});
-panelButton.addEventListener('click', function(){
-    if (modalPanel.classList.contains('active')) {
-        modalPanel.classList.remove('active');
-    } else {
-        modalPanel.classList.add('active');
+        modalBg.classList.add('active');
     }
 })
 
+panelButton.addEventListener('click', function(){
+    modal.classList.remove('active')
+    modalBg.classList.remove('active');
+    modalRun.classList.remove('active');
+})
+
+modalBg.addEventListener('click', function(){
+    modal.classList.remove('active')
+    modalBg.classList.remove('active')
+    modalRun.classList.remove('active');
+})
+panelButtonTwo.addEventListener('click', function(){
+    modalRun.classList.remove('active')
+    modalBg.classList.remove('active');
+})
